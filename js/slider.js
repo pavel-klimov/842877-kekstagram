24af89b49e1d8cae9dbe6b9cba7b6f3ba73f7987
Pavel Klimov <pavelklimovmail@yandex.ru>
2018-10-08 01:58:23 +0300

    fix

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,11 @@
 'use strict';
 
 (function () {
+  var LEFT_KEYCODE = 37;
+  var LEFT_EXTRA_KEYCODE = 100;
+  var RIGHT_KEYCODE = 39;
+  var RIGHT_EXTRA_KEYCODE = 102;
+  var EFFECT_STEP = 5;
   var width = document.querySelector('.effect-level__line').offsetWidth;
   var elementX = document.querySelector('.effect-level__line').getBoundingClientRect().x;
   var nameEffect = document.querySelector('.effects__radio:checked').value;
@@ -18,6 +23,11 @@
 
   var onEffectLevelPinKeydown = function (evt) {
     console.log(evt.keyCode);
+    if ((evt.keyCode === LEFT_KEYCODE) || (evt.keyCode === LEFT_EXTRA_KEYCODE)) {
+      changeEffectLevel(evt.clientX - EFFECT_STEP);
+    } else if ((evt.keyCode === RIGHT_KEYCODE) || (evt.keyCode === RIGHT_EXTRA_KEYCODE)) {
+      changeEffectLevel(evt.clientX + EFFECT_STEP);
+    }
     /*
     if (evt.keyCode === ESC_KEYCODE) {
       removeOverlayElement('.error');
